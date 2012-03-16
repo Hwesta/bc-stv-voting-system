75f5663c2a22b0d00669264c3da640f99bb30262
Holly Becker <hcb1@sfu.ca>
2012-03-16 01:53:43 +0000

    Adding comments, minor formatting.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@113 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- election/models.py
+++ election/models.py
@@ -85,38 +85,46 @@ KEYWORD_TYPE = (
     )
 
 class RidingKeywordList(models.Model):
+    """ The name of the keyword.  
+    
+    For example, area, population,  This is the 'title on the column'."""
     name = models.CharField(max_length=128)
     
     def __unicode__(self):
         return self.name
 
 class RidingKeywordValue(models.Model):
-   keyword = models.ForeignKey(RidingKeywordList)
-   riding = models.ForeignKey(Riding)
-   value = models.CharField(max_length=128)
+    """ The value of a keyword for a particular riding. """
+    keyword = models.ForeignKey(RidingKeywordList)
+    riding = models.ForeignKey(Riding)
+    value = models.CharField(max_length=128)
 
-   def __unicode__(self):
-       return str(self.keyword)+" "+self.value
-   class Meta:
+    def __unicode__(self):
+        return str(self.keyword)+" "+self.value
+    class Meta:
         unique_together = (('riding','keyword'))
 
 class PoliticianKeywordList(models.Model):
+    """ The name of the keyword.  
+    
+    For example, age, gender,  This is the 'title on the column'."""
     name = models.CharField(max_length=128)
 
     def __unicode__(self):
         return self.name
 
 class PoliticianKeywordValue(models.Model):
-   keyword = models.ForeignKey(PoliticianKeywordList)
-   politician = models.ForeignKey(Politician)
-   value = models.CharField(max_length=128)
+    """ The value of a keyword for a particular riding. """
+    keyword = models.ForeignKey(PoliticianKeywordList)
+    politician = models.ForeignKey(Politician)
+    value = models.CharField(max_length=128)
 
-   def __unicode__(self):
-       return str(self.keyword)+" "+self.value
-   class Meta:
+    def __unicode__(self):
+        return str(self.keyword)+" "+self.value
+    class Meta:
         unique_together = (('politician','keyword'))
 
-    
+
 STATUS_CHOICES = (
     ('BEF', 'before'),
     ('DUR', 'during '),
