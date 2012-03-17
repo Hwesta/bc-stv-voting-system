3b5e2cee84a16de20e2f1f775fba7c91081d6dad
Christopher Bolognese <cbologne@sfu.ca>
2012-03-17 21:52:32 +0000

    modified politician view and added html templates for politician
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@142 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- politicians/views.py
+++ politicians/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response
 from ridings.models import Riding
+from politicians.models import Politician
 
 # TODO Add decorators limiting access
 
@@ -7,3 +8,18 @@ from ridings.models import Riding
 # NOTE Should we have one set of functions for incumbents and candidates,
 # and just display different info based on a flag?
 
+def view_politician(request, _id):
+    p = Politician.objects.get(id=_id)
+    return render_to_response('politicians/politician.html',{'politician': p})
+	
+def view_candidates(request):
+    p = Politician.objects.filter(candidate_riding__isnull=False)
+    return render_to_response('politicians/politicians.html',{'politicians': p,'type':str("Candidates")})
+
+def view_incumbents(request):
+    p = Politician.objects.filter(incumbent_riding__isnull=False)
+    return render_to_response('politicians/politicians.html',{'politicians': p,'type':str("Incumbents")})
+
+def view_politicians(request):
+    p = Politician.objects.all()
+    return render_to_response('politicians/politicians.html',{'politicians': p,'type':str("Politicians")})
