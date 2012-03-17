d2e3dc9b8e9ed936035263ab83841a4b59b26eaf
alphai <alphai@sfu.ca>
2012-03-17 22:27:25 +0000

    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@145 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- keywords/views.py
+++ keywords/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render_to_response
-from riding.models import Riding
+from ridings.models import Riding
 from politicians.models import Politician
 
 # TODO Add decorators limiting access
 
 # Keyword Management
 
-
-
+def keywordDisplay(request):
+    keyList = RidingKeywordList.object.all
+    return render_to_response('keywords/view.html', {'keyword_list' : keyList})
