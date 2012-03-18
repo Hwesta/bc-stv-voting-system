6594a29aa9073082d23fb7436f46084d26a050b7
aabaya <aabaya@sfu.ca>
2012-03-18 00:02:36 +0000

    Keyword stuff
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@152 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- keywords/views.py
+++ keywords/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render_to_response
 from ridings.models import Riding
 from politicians.models import Politician
+from keywords.models import *
 
 # TODO Add decorators limiting access
 
 # Keyword Management
 
 def keywordDisplay(request):
-    keyList = RidingKeywordList.object.all
+    keyList = RidingKeywordList.objects.all()
     return render_to_response('keywords/view.html', {'keyword_list' : keyList})
--- urls.py
+++ urls.py
@@ -21,5 +21,9 @@ urlpatterns = patterns('',
     url(r'^election/$', 'election.views.view_election'),
     url(r'^ballots/$', 'ballots.views.view_ballots'),
     url(r'^ridings/$', 'ridings.views.view_all_ridings'),
+
+    url(r'^keywords/$', 'keywords.views.keywordDisplay'),
+
     url(r'^ridings/(\d+)/$', 'ridings.views.view_riding'),
+
 )
