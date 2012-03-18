293f62b0120cca69c8170b83e127b6a87b9469e3
Christopher Bolognese <cbologne@sfu.ca>
2012-03-18 01:34:04 +0000

    modified add politician template and politicians view
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@162 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- politicians/views.py
+++ politicians/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from ridings.models import Riding
 from politicians.models import Politician, PoliticianForm
 
@@ -28,9 +30,10 @@ def add_politician(request):
     if request.method == 'POST':
 	form = PoliticianForm(request.POST)
 	if form.is_valid():
-	    return HttpResponseRedirect('politicians/politicians.html')
+	    form.save()
+	    return HttpResponseRedirect(reverse(view_politicians))
     else:
 	form = PoliticianForm()
-    return render_to_response('politicians/add_politician.html', 		{ 'form':form,})
+    return render(request, 'politicians/add_politician.html', 		{ 'form':form,})
 
 
