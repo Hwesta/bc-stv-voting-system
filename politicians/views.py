324ba4496005ea2f1f9f4accf9f7d9e21667851b
Justin Symonds <justinjfsymonds@hotmail.com>
2012-03-29 18:54:39 +0000

    modified view for politicians to pass forward the riding id. affected templates and urls.py were fixed
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@338 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- politicians/views.py
+++ politicians/views.py
@@ -11,10 +11,14 @@ from keywords.models import PoliticianKeywordValue, PoliticianKeywordList
 # NOTE Should we have one set of functions for incumbents and candidates,
 # and just display different info based on a flag?
 
-def view_politician(request, p_id):
+def view_politician(request, r_id, p_id):
     p = Politician.objects.get(id=p_id)
     k = PoliticianKeywordValue.objects.filter(politician=p)
-    return render(request, 'politicians/politician.html',{'politician': p, 'keywords': k})
+    return render(request, 'politicians/politician.html',
+	{'politician': p,
+	 'keywords': k,
+	 'r_id': r_id
+	})
 
 def view_candidates(request):
     p = Politician.objects.filter(candidate_riding__isnull=False)
@@ -30,32 +34,41 @@ def view_incumbents(request):
          'type':str("Incumbents")
          })
 
-def view_politicians(request):
+def view_politicians(request, r_id):
     p = Politician.objects.all()
     return render(request, 'politicians/politicians.html',
         {'politicians': p,
-         'type':str("Politicians")
+         'type':str("Politicians"),
+	 'r_id': r_id
+	})
+
+def view_deleted_politicians(request, r_id):
+    p = Politician.objects.all()
+    return render(request, 'politicians/deleted_politicians.html',
+        {'politicians': p,
+         'type':str("Deleted Politicians"),
+	 'r_id': r_id
          })
 
-def add_politician(request):
+def add_politician(request, r_id):
     if request.method == 'POST':
         form = PoliticianForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse(view_politicians))
+            return HttpResponseRedirect(reverse(view_politicians, args=(r_id)))
     else:
         form = PoliticianForm()
-    return render(request, 'politicians/add_politician.html', { 'form':form,})
+    return render(request, 'politicians/add_politician.html', { 'form':form, 'r_id': r_id})
 
-def modify_politician(request, p_id):
+def modify_politician(request, r_id, p_id):
     politician = Politician.objects.get(id=p_id)
     if request.method == 'POST':
         form = PoliticianForm(request.POST,instance=politician)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse(view_politicians))
+            return HttpResponseRedirect(reverse(view_politicians, args=(r_id)))
     else:
         form = PoliticianForm(instance=politician)
-    return render(request, 'politicians/modify_politician.html', { 'form':form, 'politician':politician})
+    return render(request, 'politicians/modify_politician.html', { 'form':form, 'politician':politician, 'r_id': r_id})
 
 
