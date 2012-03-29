39abc7c0916384ce73482dbe8913d6676d1f1a3f
Dale Floer <dalefloer@gmail.com>
2012-03-29 09:42:11 +0000

    This was part of the last commit. Oops. (exception handling and validation)
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@333 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- ballots/models.py
+++ ballots/models.py
@@ -4,6 +4,8 @@ from ridings.models import Poll
 from jsonfield import JSONField
 from django.forms import ModelForm
 from django.contrib.auth.models import User
+import json
+
 
 class Ballot(models.Model):
     
@@ -22,23 +24,30 @@ class Ballot(models.Model):
     def __unicode__(self):
         return str(self.ballot_num)+", "+str(self.verified)+", "+str(self.vote)
 
-    def clean_vote(self):
-        """
-        Sum from 1..n->(n*(n+1))/2
-        This means that if a ballot is valid, then the sum of the entries should equal that.
-        """
-        vote_data=self.cleaned_data['vote']
-        print vote_data
-        
-        return vote_data
-
-    def clean_entered_by(self):
-
-        
-        return self.clean_data['entered_by']
+def ids_sequential_and_start_at_1(lst):
+    return set(lst) == set(range(1,len(lst)+1))
             
 
 class BallotForm(ModelForm):
+
+
+
+
+    def clean_vote(self):
+        vote_data=json.loads(self.cleaned_data['vote'])
+        #try:
+        vote = map(int, vote_data.keys())
+        if not ids_sequential_and_start_at_1(vote):
+            raise forms.ValidationError("Non-sequential vote entered.")
+        #except:
+            #raise forms.ValidationError("...")
+        return vote_data
+
+    '''def clean_entered_by(self):
+
+        
+        return self.clean_data['entered_by']'''
+    
     class Meta:
         model = Ballot
         fields = ('poll', 'ballot_num', 'vote', 'spoiled')
