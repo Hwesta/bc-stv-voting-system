06d7a7a52e009a247dd933876b0f92ff1ac66fb4
Dale Floer <dalefloer@gmail.com>
2012-03-29 07:04:39 +0000

    Added a entered_by and ballot_hash field to the ballots. You'll need to recreate your db for this to work. Note, these aren't currently doing anything. Also, I'm working on input validation for the ballots. It currently doesn't work.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@330 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- ballots/models.py
+++ ballots/models.py
@@ -3,6 +3,7 @@ from django import forms
 from ridings.models import Poll
 from jsonfield import JSONField
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 class Ballot(models.Model):
     
@@ -14,9 +15,28 @@ class Ballot(models.Model):
     spoiled = models.BooleanField(default=False)
     # JSONField will not load back properly, and we are decoding the contents anyway
     vote = models.TextField()
+    entered_by = models.ForeignKey(User, null=True, blank=True)
+    ballot_hash = models.CharField(max_length=128, null=True, blank=True)
+    
 
     def __unicode__(self):
         return str(self.ballot_num)+", "+str(self.verified)+", "+str(self.vote)
+
+    def clean_vote(self):
+        """
+        Sum from 1..n->(n*(n+1))/2
+        This means that if a ballot is valid, then the sum of the entries should equal that.
+        """
+        vote_data=self.cleaned_data['vote']
+        print vote_data
+        
+        return vote_data
+
+    def clean_entered_by(self):
+
+        
+        return self.clean_data['entered_by']
+            
 
 class BallotForm(ModelForm):
     class Meta:
