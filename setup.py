06bac0455c0484196fd7c9fdeeb6e32e7c81643e
Dale Floer <dalefloer@gmail.com>
2012-04-05 18:39:55 +0000

    Updated the setup to reflect new test data.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@415 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 subprocess.call(["python", "manage.py", "syncdb", "--noinput"])
 
-files=[0,1,3,4,5,6,7,8]
+files=[0,1,2,3,4,5,6,7,8]
 
 for value in files:
     subprocess.call(["python", "manage.py", "loaddata", "test_data_00"+str(value)+".json"])
