d05597987d8269a4fc5596b8eb0fbdf324f41751
Dale Floer <dalefloer@gmail.com>
2012-04-05 02:59:19 +0000

    Fix docs.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@389 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- politicians/models.py
+++ politicians/models.py
@@ -5,14 +5,13 @@ from django.forms import ModelForm
 class Politician(models.Model):
     name = models.CharField(max_length=128)
     party = models.CharField(max_length=128)
-    # This should be null if the politician is not a candidate
+    # This should be NULL if the politician is not a candidate
     candidate_riding = models.ForeignKey(Riding, null=True, blank=True,
         related_name="candidate_riding")
-    # This should be null if the politician is not an incumbent
+    # This should be NULL if the politician is not an incumbent
     incumbent_riding = models.ForeignKey(Riding, null=True, blank=True,
         related_name="incumbent_riding")
-    delete = models.BooleanField()
-    delete.default = False
+    delete = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.name+", "+self.party+" candidate in "+str(self.candidate_riding)+" incumbent in "+str(self.incumbent_riding)
