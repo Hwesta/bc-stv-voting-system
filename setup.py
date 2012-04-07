c9db61ee0ac92b384757aab7758725888d2e4f7b
Holly Becker <hcb1@sfu.ca>
2012-04-07 03:32:34 +0000

    Fixed my broken setup.py
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@534 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 import subprocess
 
-PYTHON_COMMAND="python"
+PYTHON_COMMAND="python2"
 
 subprocess.call([PYTHON_COMMAND, "manage.py", "syncdb", "--noinput"])
 
-files=[00,01,02,03,04,05,06,07,08,09,10]
+files=['00','01','02','03','04','05','06','07','08','09','10']
 
 for value in files:
-    jsonfile="test_data_0"+str(value)+".json"
+    jsonfile="test_data_0"+value+".json"
     print "Loading",jsonfile
     subprocess.call([PYTHON_COMMAND, "manage.py", "loaddata", jsonfile])
