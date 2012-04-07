576284da0ae8995b2510ca84f0fd48d4ad0996bc
Christopher Bolognese <cbologne@sfu.ca>
2012-04-07 02:00:10 +0000

    fixed naming error
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@512 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- politicians/views.py
+++ politicians/views.py
@@ -14,11 +14,11 @@ from election.models import define_view_permissions, permissions_or, permissions
 # NOTE Should we have one set of functions for incumbents and candidates,
 # and just display different info based on a flag?
 
-@user_passes_test(permissions_or(define_view_permissions(['EO'],['BEF','DUR','AFT']), define_view_permissions(['REP'],['DUR'])))
+
 def view_all_politicians(request):
     p = Politician.objects.all().filter(delete=False)
     return render(request, 'politicians/politicians.html',
-	{'politicans': p})
+	{'politicians': p})
 
 @user_passes_test(permissions_or(define_view_permissions(['EO'],['BEF','DUR','AFT']), define_view_permissions(['REP'],['DUR'])))
 def view_politician(request, r_id, p_id):
