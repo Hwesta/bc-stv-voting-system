c843d1d5e587ac5d1c3e1d0a61cd53d23a9e9c74
Dale Floer <dalefloer@gmail.com>
2012-04-07 20:57:56 +0000

    Use printf format strings.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@559 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ PYTHON_COMMAND="python"
 
 subprocess.call([PYTHON_COMMAND, "manage.py", "syncdb", "--noinput"])
 
-files=['00','01','02','03','04','05','06','07','08','09','10']
+files=[0,1,2,3,4,5,6,7,8,9,10]
 
 for value in files:
-    jsonfile="test_data_0"+value+".json"
+    jsonfile="test_data_%03d.json" % (value)
     print "Loading",jsonfile
     subprocess.call([PYTHON_COMMAND, "manage.py", "loaddata", jsonfile])
