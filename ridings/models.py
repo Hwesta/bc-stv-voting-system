113e3d25fe2b65ff0145211bfc7f42100349cf45
Dale Floer <dalefloer@gmail.com>
2012-04-08 00:20:04 +0000

    Implement a standalone riding verify, refactored from verify_riding view. Fix whitespace in Poll.save()
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@568 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- ridings/models.py
+++ ridings/models.py
@@ -73,7 +73,167 @@ class Riding(models.Model):
         if deleted_poll_list != "":
             s += ", excluding %s due to deletion" % (deleted_poll_list, )
         return s
-    
+   
+    def verify(self):
+        # Do not put this import at the toplevel!
+        # It will break because ballots/models.py imports this file!
+        from ballots.models import Ballot
+        # General notes:
+        # IMPORTANT:
+        # All ballot fetches MUST contain exactly one of the following
+        # - exclude(state='R')
+        # - filter(state='x') where x != R
+        # Otherwise you will get old recount ballots as well!
+        # It needs to be inside the Raw SQL as well for correct processing
+        poll_ids = self.poll_set.values('id').distinct()
+        poll_ids_str = ",".join([str(x['id']) for x in poll_ids])
+
+        # Pass 1: All ballots with state NOT 'recount', that exist only once
+        ballots_entered_only_once = Ballot.objects.raw(" \
+            SELECT id FROM ballots_ballot \
+            INNER JOIN ( \
+                SELECT \
+                    ballot_num, \
+                    COUNT(ballot_num) AS cnt \
+                FROM ballots_ballot \
+                WHERE state = 'U' AND poll_id IN (%s) \
+                GROUP BY ballot_num \
+                HAVING COUNT(ballot_num) < 2 \
+            ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+        " % (poll_ids_str, ) )
+        ids = map(lambda b: b.id, ballots_entered_only_once)
+        ballots_entered_only_once = Ballot.objects.filter(state='U').filter(id__in=ids).filter(poll__in=poll_ids)
+
+        # Pass 2: Exclude pass 1, No mix of unverified/correct, unverified/correct
+        ballot_invalid_state_mix = Ballot.objects.raw(" \
+            SELECT id FROM ballots_ballot \
+            INNER JOIN ( \
+                SELECT \
+                    ballot_num, \
+                    COUNT(ballot_num) AS cnt \
+                FROM ballots_ballot \
+                WHERE state != 'R' AND poll_id IN (%s) \
+                GROUP BY ballot_num, state \
+                HAVING COUNT(ballot_num)=1 AND state='U' \
+            ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+        " % (poll_ids_str, ))
+        ids = map(lambda b: b.id,ballot_invalid_state_mix)
+        ballot_invalid_state_mix = Ballot.objects.exclude(state='R').exclude(id__in=ballots_entered_only_once).filter(id__in=ids).filter(poll__in=poll_ids)
+
+        # Pass 3: Completion check
+        # If this AND the above lists are empty
+        # We have completed all validation!
+        # SELECT COUNT(*) AS cnt
+        # FROM ballots_ballot
+        # WHERE state = 'unverifed';
+        unverified_count = Ballot.objects.filter(state='U').filter(poll__in=poll_ids).count()
+
+        bad_ballots = list(ballots_entered_only_once) + list(ballot_invalid_state_mix)
+
+        if unverified_count == 0:
+            return { 
+                    'single entry': ballots_entered_only_once,
+                    'invalid_state_mix': ballot_invalid_state_mix,
+                    'single_ro_only': [],
+                    'auto_ballots': [], 
+                    'manual_ballots': [],
+                    }
+
+        # Pass 4: Double-entry check
+        # Each unverified ballot should exist twice by different RO
+        # Yes, the docs say NOT to use string replacement,
+        # But .raw params does not support lists!
+        bad_ballot_nums = set(map(lambda b: b.ballot_num, bad_ballots))
+        bad_ballot_nums_str_clause = ''
+        if len(bad_ballot_nums) > 0:
+            bad_ballot_nums_str = ','.join(map(str,bad_ballot_nums))
+            bad_ballot_nums_str_clause = 'AND ballot_num NOT IN (%s)' %  (bad_ballot_nums_str ,)
+        ballots_no_different_ro = Ballot.objects.raw(" \
+                SELECT id FROM ballots_ballot \
+                INNER JOIN ( \
+                SELECT \
+                ballot_num, \
+                COUNT(DISTINCT entered_by_id) AS cnt \
+                FROM ballots_ballot \
+                WHERE state = 'U' AND poll_id IN (%s) %s\
+                GROUP BY ballot_num \
+                HAVING COUNT(DISTINCT entered_by_id) < 2 \
+                ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+                " % (poll_ids_str, bad_ballot_nums_str_clause ,))
+        ids = map(lambda b: b.id, ballots_no_different_ro)
+        #ballots_no_different_ro_ballot_num = map(lambda b: b.ballot_num, ballots_no_different_ro)
+        ballots_no_different_ro = Ballot.objects.filter(state='U').filter(id__in=ids).filter(poll__in=poll_ids)
+
+        bad_ballots = bad_ballots + list(ballots_no_different_ro)
+
+        # Pass 5: Conflict resolution - Automatic 
+        # If we grab unverified ballots not caught so far
+        # And they have matching (ballot_num, spoiled) or (ballot_num, vote)
+        # We can auto-verify them
+        bad_ballot_nums = set(map(lambda b: b.ballot_num, bad_ballots))
+        bad_ballot_nums_str_clause = ''
+        if len(bad_ballot_nums) > 0:
+            bad_ballot_nums_str = ','.join(map(str,bad_ballot_nums))
+            bad_ballot_nums_str_clause = 'AND ballot_num NOT IN (%s)' %  (bad_ballot_nums_str ,)
+        ballots_spoiled_auto_approve = Ballot.objects.raw(" \
+                SELECT id FROM ballots_ballot \
+                INNER JOIN ( \
+                SELECT \
+                ballot_num, \
+                COUNT(spoiled) AS cnt, \
+                COUNT(DISTINCT spoiled) AS cnt_d \
+                FROM ballots_ballot \
+                WHERE state='U' AND spoiled=1 AND poll_id IN (%s) %s \
+                GROUP BY ballot_num, spoiled \
+                HAVING COUNT(spoiled)=2 AND COUNT(DISTINCT spoiled)=1 \
+            ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+        " % (poll_ids_str, bad_ballot_nums_str_clause ,))
+        ballots_vote_auto_approve = Ballot.objects.raw(" \
+            SELECT id FROM ballots_ballot \
+            INNER JOIN ( \
+                SELECT \
+                    ballot_num, \
+                    COUNT(vote) AS cnt, \
+                    COUNT(DISTINCT vote) AS cnt_d \
+                FROM ballots_ballot \
+                WHERE state='U' AND spoiled=0 AND poll_id IN (%s) %s \
+                GROUP BY ballot_num, vote \
+                HAVING COUNT(vote)=2 AND COUNT(DISTINCT vote)=1 \
+            ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+        " % (poll_ids_str, bad_ballot_nums_str_clause ,))
+        ids = map(lambda b: b.id, ballots_spoiled_auto_approve) + map(lambda b: b.id, ballots_vote_auto_approve)
+        ballots_auto_approve = Ballot.objects.filter(state='U').filter(id__in=ids).filter(poll__in=poll_ids)
+
+        # Pass 6: Conflict resolution - Manual
+        # Remaining unverified items
+        # - 2 unique ROs
+        # - (2 unique votes) OR (2 uniques spoiled)
+        # - Exclude ballots_auto_approve
+        ballots_manual_approve = Ballot.objects.raw(" \
+            SELECT id FROM ballots_ballot \
+            INNER JOIN ( \
+                SELECT \
+                    ballot_num, \
+                    COUNT(DISTINCT entered_by_id) AS cnt_ro, \
+                    COUNT(DISTINCT vote) as cnt_v, \
+                    COUNT(DISTINCT spoiled) as cnt_s \
+                FROM ballots_ballot \
+                WHERE state = 'U' AND poll_id IN (%s) \
+                GROUP BY ballot_num \
+                HAVING COUNT(DISTINCT entered_by_id)= 2 AND (COUNT(DISTINCT vote) = 2 OR COUNT(DISTINCT spoiled) = 2) \
+            ) q1 ON q1.ballot_num=ballots_ballot.ballot_num \
+        " % (poll_ids_str, ))
+        ids = map(lambda b: b.id, ballots_manual_approve)
+        ballots_manual_approve = Ballot.objects.exclude(id__in=ballots_auto_approve).filter(state='U').filter(id__in=ids).filter(poll__in=poll_ids)
+
+        return {
+            'single_entry': ballots_entered_only_once,
+            'invalid_state_mix': ballot_invalid_state_mix,
+            'single_ro_only': ballots_no_different_ro,
+            'auto_ballots': ballots_auto_approve, 
+            'manual_ballots': ballots_manual_approve,
+        }
+
     # Private
     def _poll_minmaxcount(self):
         return self.poll_set.aggregate(maxpoll=Max('poll_num'),minpoll=Min('poll_num'),count=Count('id'))
@@ -96,15 +256,15 @@ class Poll(models.Model):
         #return str(self.riding)+", "+str(self.polling_stn)+"
 
     def close(self):
-	# riding poll belongs to
-	riding = Riding.objects.get(name=self.riding)
-	self.active = False
-	self.save()
-	# the # of polls belonging to the riding that are still active
-	num_rem_polls = Poll.objects.filter(riding=self.riding, active=True).exclude(delete=True).count()
-	if num_rem_polls == 0:
-	    riding.active = False
-	    riding.save()
+        # riding poll belongs to
+        riding = Riding.objects.get(name=self.riding)
+        self.active = False
+        self.save()
+        # the # of polls belonging to the riding that are still active
+        num_rem_polls = Poll.objects.filter(riding=self.riding, active=True).exclude(delete=True).count()
+        if num_rem_polls == 0:
+            riding.active = False
+            riding.save()
 
     def save(self, *args, **kwargs):
         # Is this a create
