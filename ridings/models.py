d74a87df35e9cc179feaee113069e8125b14927a
Dale Floer <dalefloer@gmail.com>
2012-04-08 00:20:16 +0000

    Add function to return true/false if safe to close a riding.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@569 a35e499e-f4d2-495b-8bd3-10c317ad12fd

--- ridings/models.py
+++ ridings/models.py
@@ -73,7 +73,12 @@ class Riding(models.Model):
         if deleted_poll_list != "":
             s += ", excluding %s due to deletion" % (deleted_poll_list, )
         return s
-   
+  
+    def safe_to_close(self):
+        verify_result = self.verify()
+        unverified_ballots = [ballot for tmplist in verify_result.values() for ballot in tmplist]
+        return len(unverified_ballots) > 0
+
     def verify(self):
         # Do not put this import at the toplevel!
         # It will break because ballots/models.py imports this file!
