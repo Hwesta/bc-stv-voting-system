104c4ecb4922cf6a46c856a2467bf96b09f63d88
Dale Floer <dalefloer@gmail.com>
2012-04-09 00:02:29 +0000

    Convert dumpdata to Python script for windows use.
    git-svn-id: https://punch.cs.sfu.ca/svn/CMPT275-1121-g-group2/trunk@614 a35e499e-f4d2-495b-8bd3-10c317ad12fd

---
+++ dumpdata.py
@@ -0,0 +1,18 @@
+from django.core.management import setup_environ
+from django.core import management
+from StringIO import StringIO
+import settings
+setup_environ(settings)
+import os
+for idx, tl in enumerate(settings.TABLE_DUMP_ORDER):
+    output_file = "test_data_%03d.json" % (idx, )
+    buf = StringIO()
+    management.call_command('dumpdata', *tl, verbosity=1, indent=2, format='json', stdout=buf)
+    if buf.pos > 4:
+        buf.write("\n")
+        f = open(output_file, 'w')
+        buf.seek(0)
+        f.write(buf.read())
+        f.close()
+    else:
+        os.unlink(output_file)
